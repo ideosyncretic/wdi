21a34e3da974bba6a4052e3710dd1be8daca20db
Sprazzeus <sprazzeus@gmail.com>
2016-06-07 17:14:02 +0800

    game: writing mastermind logic

---
+++ 01-Unit1/00-project1b/mastermind.js
@@ -0,0 +1,16 @@
+
+// generate sequence of 4 randomized pins. factors to randomize are color and index position.
+secretPins = []
+
+function generatePins() {
+  var pinColor;
+  if (math.Random() < 0.3) {
+    pinColor = blue
+  };
+  if ()
+  secretPins.push()
+}
+
+// correct position
+// correct color
+console.log(math.Random())
