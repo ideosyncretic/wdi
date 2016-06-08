fff83134f609e5a5aef82c6ed2fad9ca5d7787a5
Sprazzeus <sprazzeus@gmail.com>
2016-06-08 10:00:10 +0800

    quiz: corrected line 119 break. mastermind progress

--- 01-Unit1/00-project1b/mastermind.js
+++ 01-Unit1/00-project1b/mastermind.js
@@ -1,16 +1,51 @@
 
-// generate sequence of 4 randomized pins. factors to randomize are color and index position.
-secretPins = []
+// generate random sequence of 4 colored pins.
+secretPins = new Array();
 
 function generatePins() {
   var pinColor;
-  if (math.Random() < 0.3) {
-    pinColor = blue
-  };
-  if ()
-  secretPins.push()
+  var randomNumber;
+  for (var i = 0; i < 4; i++) {
+    randomNumber = Math.random();
+    console.log(randomNumber)
+    if (randomNumber < 0.25) {
+      pinColor = 0 // blue
+    };
+    if (randomNumber > 0.25) {
+      pinColor = 1 // red
+    };
+    if (randomNumber > 0.5) {
+      pinColor = 2 // yellow
+    };
+    if (randomNumber > 0.75 ) {
+      pinColor = 3 // green
+    };
+    secretPins.push(pinColor)
+  }
 }
 
-// correct position
-// correct color
-console.log(math.Random())
+generatePins();
+
+console.log(secretPins)
+
+
+// generate key pins: white for correct color, and black for correct position + correct color. singular instances only, whereupon each player guess correspond with a secret pin.
+
+
+// player has 12 guesses.
+var guesses = new Array(12)
+
+// player inputs guesses of 4 colored pins.
+
+var guess = new Array(4);
+
+function choosePins() {
+  if
+  $("#blue").click( function() {
+    guess.push(0)
+  })
+
+}
+
+
+// end code
