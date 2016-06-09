3fe93788acda3aaa16c3ae97fd0fd718e657456b
Sprazzeus <sprazzeus@gmail.com>
2016-06-09 11:15:02 +0800

    mastermind: currentGuess separation working, checkGuess in progress. minor CSS tweaks.

--- 01-Unit1/00-project1b/mastermind.js
+++ 01-Unit1/00-project1b/mastermind.js
@@ -27,38 +27,61 @@ generateAnswer();
 console.log(answer);
 
 
-// generate key pins: white for correct color, and black for correct position + correct color. singular instances only, whereupon each player guess correspond with a secret pin.
-
-
-// player has 12 guesses. each guess comprises 4-pin sequence.
+// player has 10 guesses, each comprises 4-pin sequence.
 
 var guesses = [];
-var guess = [];
-var currentPin = 0;
-var completeGuess = false;
-var totalGuesses = 0;
+var currentGuess = [];
+var currentPinNumber = 0;
+var currentGuessNumber = 0;
+var guessChecked = false;
 
 $(function() {
 
 function playerGuess() {
   $(".picker").click(function() {
-    if ( (guess.length < 4) && (currentPin < 40) ) {
-      guess.push($(this).attr("class").split(" ")[0])
-      $("button").eq(currentPin).addClass($(this).attr("class").split(" ")[0])
-      console.log("Guess = " + guess)
-      currentPin++;
-      completeGuess = false;
+    if ( (currentGuess.length < 4 && (currentPinNumber < 40)) ) {
+      currentGuess.push($(this).attr("class").split(" ")[0])
+      $("div.guess > button").eq(currentPinNumber).addClass($(this).attr("class").split(" ")[0])
+      console.log("currentGuess (" + currentGuess.length + ") " + currentGuess)
+      currentPinNumber++;
     }
-    if (guess.length === 4) {
-      guesses.push([guess])
-      console.log("Guesses = " + guesses)
-      guess = [];
-      completeGuess = true;
-      totalGuesses += 1;
-      console.log("New complete guess! Total = " + totalGuesses)
+  })
+
+};
+
+
+playerGuess();
+
+// should clear individual pin in current guess.
+
+function clearGuess() {
+  $("#clear").click( function() {
+    if (currentGuess.length !== 0) {
+    $("div.guess > button").eq(currentPinNumber-1).removeClass()
+    currentPinNumber -= 1;
+    currentGuess.pop();
+    console.log("currentGuess (" + currentGuess.length + ") " + currentGuess)
     }
   })
 };
+
+clearGuess();
+
+
+function checkGuess() {
+  $("#check").click(function() {
+    if (currentGuess.length === 4) {
+      guesses.push([currentGuess])
+      console.log("Guesses = " + guesses)
+      currentGuess = [];
+    }
+  })
+}
+
+checkGuess();
+
+// generate key pins: white for correct color, and black for correct position + correct color. singular instances only, whereupon each player guess correspond with a secret pin.
+
 
 function gameOver() {
   if (guesses.length === 10) {
@@ -66,23 +89,5 @@ function gameOver() {
   }
   return false;
 };
-
-playerGuess();
-
-function clearGuess() {
-  $("#clear").click( function() {
-    if (completeGuess) {
-      for (var i = 0; i < 4; i++) {
-        $("button").eq(currentPin-1).removeClass()
-        currentPin -= 1;
-      }
-    completeGuess = false;
-    totalGuesses -= 1;
-    console.log("clearGuess ran, total = " + totalGuesses);
-    }
-  })
-};
-
-clearGuess();
 
 });// end code
